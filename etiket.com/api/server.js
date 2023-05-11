86dc04d8c3c4cdf776b9576c283704e165efdc70
Team Ice Code <132735983+TeamIceCode@users.noreply.github.com>
2023-05-12 00:02:35 +0800

    Add files via upload

--- etiket.com/api/server.js
+++ etiket.com/api/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // middleware logger
 app.use((req, res, next) => {
     const start = Date.now();
-    res.header('Access-Control-Allow-Origin', 'https://teamicecode.github.io/etiket.com/registration');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');  
     next();
